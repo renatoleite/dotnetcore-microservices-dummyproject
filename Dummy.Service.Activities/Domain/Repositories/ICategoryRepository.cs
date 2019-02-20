c898c29e50d22769a03e2bd5e0f33043d0634d3b
Renato Leite <hr2.renato@gmail.com>
2019-02-19 22:35:28 -0300

    Creating models and repositories

---
+++ Dummy.Service.Activities/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Dummy.Service.Activities.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dummy.Service.Activities.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> GetAsync(string name);
+        Task<IEnumerable<Category>> SearchAsync();
+        Task AddAsync(Category category);
+    }
+}
