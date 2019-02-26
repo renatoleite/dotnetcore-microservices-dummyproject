f7ce358e656614e149f16173e5983dbc33e27700
Renato Leite <hr2.renato@gmail.com>
2019-02-26 13:30:02 -0300

    Creating new endpoints to filter activities

--- Dummy.Api/Controllers/ActivitiesController.cs
+++ Dummy.Api/Controllers/ActivitiesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Dummy.Api.Repositories;
 using Dummy.Common.Commands;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -12,13 +13,40 @@ using RawRabbit;
 namespace Dummy.Api.Controllers
 {
     [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ActivitiesController : Controller
     {
         private readonly IBusClient _busClient;
+        private readonly IActivityRepository _repository;
 
-        public ActivitiesController(IBusClient busClient)
+        public ActivitiesController(
+            IBusClient busClient,
+            IActivityRepository repository)
         {
-            this._busClient = busClient;
+            _busClient = busClient;
+            _repository = repository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var activities = await _repository.SearchAsync(Guid.Parse(User.Identity.Name));
+
+            return Json(activities.Select(x => new { x.Id, x.Name, x.Category, x.CreatedAt }));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var activity = await _repository.GetAsync(id);
+
+            if (activity == null)
+                return NotFound();
+
+            if (activity.UserId != Guid.Parse(User.Identity.Name))
+                return Unauthorized();
+
+            return Json(activity);
         }
 
         [HttpPost("")]
