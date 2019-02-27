91d0da049204dc8546520acf40c21cbc2bc97714
Renato Leite <hr2.renato@gmail.com>
2019-02-27 10:13:46 -0300

    Creating unit and integration tests to Dummy Services Activities

---
+++ Dummy.Service.Activities/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dummy.Service.Activities.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get() => Content("Responding");
+    }
+}
---
+++ Tests/Dummy.Service.Activities.Tests/Unit/Services/ActivityServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Dummy.Service.Activities.Domain.Models;
+using Dummy.Service.Activities.Domain.Repositories;
+using Dummy.Service.Activities.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dummy.Service.Activities.Tests.Unit.Services
+{
+    public class ActivityServiceTests
+    {
+        [Fact]
+        public async Task activity_service_add_async_should_succeed()
+        {
+            // Mocks
+            var category = "test";
+            var activityRepositoryMock = new Mock<IActivityRepository>();
+            var categoryRepositoryMock = new Mock<ICategoryRepository>();
+
+            categoryRepositoryMock.Setup(x => x.GetAsync(category))
+                .ReturnsAsync(new Category(category));
+
+            var activityService = new ActivityService(
+                activityRepositoryMock.Object,
+                categoryRepositoryMock.Object);
+
+            var id = Guid.NewGuid();
+            await activityService.AddAsync(id, Guid.NewGuid(), category, "activity", "description", DateTime.UtcNow);
+
+            categoryRepositoryMock.Verify(x => x.GetAsync(category), Times.Once);
+            activityRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Activity>()), Times.Once);
+        }
+    }
+}
